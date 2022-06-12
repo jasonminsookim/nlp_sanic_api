9261145530d25a33be783271e133338e50003dce
Jason Kim <34352746+jasonminsookim@users.noreply.github.com>
2022-06-12 03:49:17 +0000

    SentimentAnalsysi data model

--- src/models/sentiment_analysis.py
+++ src/models/sentiment_analysis.py
@@ -1,2 +1,9 @@
-from pydantic import BaseModel 
+from pydantic import BaseModel, UUID4
+
+class SentimentAnalysis(BaseModel):
+    document_text: str
+    polarity_score: float
+    subjectivity_score: float
+    n_gram: int
+    text_id: UUID4
 
